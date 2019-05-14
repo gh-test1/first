91217ab3a34990bc0b75906633de1c80b37f2386
gh-test1 <gh-test1@yandex.ru>
2019-05-14 09:52:41 +0300

    change 1

--- 1.cpp
+++ 1.cpp
@@ -1,7 +1,38 @@
+
 #include <iostream>
+#include <string>
+
 int main()
 {
-	int A = 10;
-	std:: cout << "Hello, A = " << (A + 1)<< std:: endl;
-	return 0;
+	std::string number;
+
+	std::cin >> number;
+
+
+	int _size = number.size();
+	int last_num = (int)number[_size - 1] - 48;
+	int pre_last_num = 0;
+
+	if (_size > 1) {
+		pre_last_num = (int)number[_size - 2] - 48;
+	}
+
+	switch (last_num)
+	{
+	case 1:
+		if (pre_last_num != 1) {
+			std::cout << number << " " << "derevo" << std::endl;
+			break;
+		}
+	case 2:
+	case 3:
+	case 4:
+		if (pre_last_num != 1) {
+			std::cout << number << " " << "dereva" << std::endl;
+			break;
+		}
+	default:
+		std::cout << number << " " << "derev'ev" << std::endl;
+		break;
+	}
 }
